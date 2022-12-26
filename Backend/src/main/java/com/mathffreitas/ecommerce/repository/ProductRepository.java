03e4e5c0920cb20c27d08a074d7880c988042f01
Math <mathffreitas@hotmail.com>
2022-12-26 15:09:10 -0300

    FTEND-02
    Integrating Angular and Spring Boot

--- Backend/src/main/java/com/mathffreitas/ecommerce/repository/ProductRepository.java
+++ Backend/src/main/java/com/mathffreitas/ecommerce/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.mathffreitas.ecommerce.repository;
 import com.mathffreitas.ecommerce.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "product", path = "products")
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 }
