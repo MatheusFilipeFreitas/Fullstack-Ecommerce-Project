af8abb6e28685e3632026fd082e4c8aa8fe82db8
Math <mathffreitas@hotmail.com>
2022-12-26 14:21:30 -0300

    BKEND-02
    Creating spring data configuration

---
+++ Backend/src/main/java/com/mathffreitas/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,28 @@
+package com.mathffreitas.ecommerce.config;
+
+import com.mathffreitas.ecommerce.entity.ProductCategoryEntity;
+import com.mathffreitas.ecommerce.entity.ProductEntity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        //disable HTTP method for Product: PUT, POST and DELETE
+        config.getExposureConfiguration().forDomainType(ProductEntity.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //disable HTTP method for Product Ctegory: PUT, POST and DELETE
+        config.getExposureConfiguration().forDomainType(ProductCategoryEntity.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
