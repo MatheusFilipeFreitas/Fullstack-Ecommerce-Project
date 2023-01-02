7615049fcee1573bbb3bac76fd3b9c1ce8a523a3
Math <mathffreitas@hotmail.com>
2023-01-02 11:38:57 -0300

    BKEND-03
    Search for Products By Category

--- Backend/src/main/java/com/mathffreitas/ecommerce/repository/ProductRepository.java
+++ Backend/src/main/java/com/mathffreitas/ecommerce/repository/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.mathffreitas.ecommerce.repository;
 
 import com.mathffreitas.ecommerce.entity.ProductEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    Page<ProductEntity> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
